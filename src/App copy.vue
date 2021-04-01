99baa9c114ae3c9e078a51bb4eb378cad58584c1
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-01 10:26:49 +0700

    v-show an hien

---
+++ src/App copy.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Menu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Menu/>
+    <!--root element -->
+    <div v-show="isShow">
+      <h1>lap trinh vien</h1>
+    </div>
+
+    <button @click="isShow = !isShow ">An/hien</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Menu from './components/Menu.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Menu
-  }
-}
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
